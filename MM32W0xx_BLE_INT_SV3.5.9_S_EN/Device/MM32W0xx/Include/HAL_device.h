3e42fe8cdf4039e60a1985daf04c9ced4486c1e6
chnepeng <CP2471654180@qq.com>
2020-05-25 13:13:53 +0800

    .Schematic diagram of adding Bluetooth Demo board.

---
+++ MM32W0xx_BLE_INT_SV3.5.9_S_EN/Device/MM32W0xx/Include/HAL_device.h
@@ -0,0 +1,31 @@
+/**************************************************************************//**
+* @file HAL_device.h
+* @brief CMSIS Cortex-M Peripheral Access Layer for MindMotion
+*        microcontroller devices
+*
+* This is a convenience header file for defining the part number on the
+* build command line, instead of specifying the part specific header file.
+*
+* Example: Add "MM32W0xx_s" to your build options, to define part
+*          Add "#include "HAL_device.h" to your source files
+*
+*
+* @version 2.0.0
+*
+*
+*****************************************************************************/
+
+#ifndef __HAL_device_H
+#define __HAL_device_H
+
+#define  MM32W0xx
+
+#if defined(MM32W0xx)
+
+#include "MM32W0xx.h"
+
+#else
+#error "HAL_device.h: PART NUMBER undefined"
+#endif
+#endif /* __HAL_device_H */
+/*-------------------------(C) COPYRIGHT 2016 MindMotion ----------------------*/
---
+++ MM32W0xx_BLE_INT_SV3.5.9_S_EN/SPI_Control_BLE_Prj/HARDWARE/CALLBACK/callback.h
@@ -0,0 +1,8 @@
+#ifndef _CALLBACK_H_
+#define _CALLBACK_H_
+
+#include "HAL_conf.h"
+
+
+
+#endif
